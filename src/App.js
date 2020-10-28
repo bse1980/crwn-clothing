e54bc50c717cc20d72d2964f5e719ac1d9c9aeb6
Bobby Sojitra <bobby.sojitra@preqin.com>
2020-10-28 21:10:38 +0000

    update menu items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch> {/* renders the first matched Route */}
+        <Route exact path='/' component={HomePage} /> {/* 'exact' or none attribute */}
+        <Route path='/hats' component={HatsPage} /> { /* passing params like this. get from props*/}
+      </Switch>
     </div>
   );
 }
