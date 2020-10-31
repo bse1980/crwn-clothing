b5372e843429af846b451fdf4ae74600ea5da060
Bobby Sojitra <bobby.sojitra@preqin.com>
2020-10-31 21:34:37 +0000

    shop page, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = (props) => {
-  return (
-    <div>
-      <h1>Hats page</h1>
-    </div>
-  )
-}
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch> {/* renders the first matched Route */}
         <Route exact path='/' component={HomePage} /> {/* 'exact' or none attribute */}
-        <Route path='/hats' component={HatsPage} /> { /* passing params like this. get from props*/}
+        <Route path='/shop' component={ShopPage} /> { /* passing params like this. get from props*/}
       </Switch>
     </div>
   );
